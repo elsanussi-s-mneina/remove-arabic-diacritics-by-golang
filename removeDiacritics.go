5d34090283fb3fa717908911acff40df533e9488
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2021-03-08 20:01:02 -0600

    removal of KASRA, DAMMA, SHADDA, KASRATAN, and DAMMATAN implemented

--- removeDiacritics.go
+++ removeDiacritics.go
@@ -33,7 +33,15 @@ import (
 
 // So far the program only removes a single diacritic.
 const arabic_fathatan rune = '\u064B'
+const arabic_dammatan rune = '\u064C'
+const arabic_kasratan rune = '\u064D'
 const arabic_fatha rune = '\u064E'
+const arabic_damma rune = '\u064F'
+const arabic_kasra rune = '\u0650'
+const arabic_shadda rune = '\u0651'
+const arabic_sukun rune = '\u0652'
+const arabic_subscript_alef rune = '\u0656'
+const arabic_letter_superscript_alef rune = '\u0670'
 
 // https://golang.org/ref/spec#Rune_literals
 // https://golangbyexample.com/iterate-over-a-string-golang/
@@ -43,8 +51,16 @@ func TextFilter(text string) string {
 	for _, aRune := range text {
 		if aRune == arabic_fathatan {
 
+		} else if aRune == arabic_dammatan {
+		} else if aRune == arabic_kasratan {
+
 		} else if aRune == arabic_fatha {
 
+		} else if aRune == arabic_damma {
+
+		} else if aRune == arabic_kasra {
+
+		} else if aRune == arabic_shadda {
 		} else {
 			result += string(aRune)
 		}
--- removeDiacritics_test.go
+++ removeDiacritics_test.go
@@ -27,12 +27,55 @@ func TestTextFilterOnFathatan(t *testing.T) {
 func TestTextFilterOnFatha(t *testing.T) {
 	v := TextFilter("بَ")
 	if v != "ب" {
-		t.Error("Failed to remove FATHA")
+		t.Error("Failed to remove FATHA on BA")
 	}
 
 	v = TextFilter("سَلام")
 	if v != "سلام" {
 		t.Error("Failed to remove FATHA on word SALAAM")
+	}
+}
+
+func TestTextFilterOnWordForBall(t *testing.T) {
+	ball_nominative := "كُرَةٌ"
+	ball_accusative := "كُرَةً"
+	ball_genitive := "كُرَةٍ"
+
+	v := TextFilter(ball_nominative)
+	if v != "كرة" {
+		t.Error("Failed to remove DAMMA, FATHA, FATHATAN")
+	}
+
+	v = TextFilter(ball_accusative)
+	if v != "كرة" {
+		t.Error("Failed to remove DAMMA, FATHA, FATHATAN")
+	}
+
+	v = TextFilter(ball_genitive)
+	if v != "كرة" {
+		t.Error("Failed to remove DAMMA, FATHA, KASRATAN")
+	}
+
+}
+
+func TestTextFilterOnWordForGoodness(t *testing.T) {
+	nominative := "بِرٌّ"
+	accusative := "بِرٌّ"
+	genitive := "بِرٌّ"
+
+	v := TextFilter(nominative)
+	if v != "بر" {
+		t.Error("Failed to remove KASRA, SHADDA, DAMMATAN")
+	}
+
+	v = TextFilter(accusative)
+	if v != "بر" {
+		t.Error("Failed to remove KASRA, SHADDA, FATHATAN")
+	}
+
+	v = TextFilter(genitive)
+	if v != "بر" {
+		t.Error("Failed to remove KASRA, SHADDA, KASRATAN")
 	}
 
 }
